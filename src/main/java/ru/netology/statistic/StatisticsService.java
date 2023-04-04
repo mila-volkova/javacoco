8f4c7ce8c2ff86bb6147016787facc9e0a906f11
Neto test <lyudmila.volkova.70@mail.ru>
2023-04-04 19:18:29 +0800

    second

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,6 +1,7 @@
 package ru.netology.statistic;
 
-public class StatisticsService()
+public class StatisticsService {
+
         public long findMax(long[] incomes) {
             long currentMax = incomes[0];
             for (long income : incomes) {
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -16,5 +16,15 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
+    }  @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 60, 11, 11, 12};
+        long expected = 60;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
